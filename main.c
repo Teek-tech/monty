4551aa9a6ff761103ba45ef3e1a2ab247f401b99
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-12-15 14:51:40 -0800

    modify header

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#define  _POSIX_C_SOURCE 200809L
+#define _POSIX_C_SOURCE 200809L
 #include "monty.h"
 
 vars var;
--- opfun.c
+++ opfun.c
@@ -61,3 +61,41 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 }
 
+/**
+ * pint - Print last node
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+* pop - Delete top of list
+* @stack: Double linked list
+* @line_number: File line execution
+*/
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = *stack;
+	*stack = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = NULL;
+	free(tmp);
+}
